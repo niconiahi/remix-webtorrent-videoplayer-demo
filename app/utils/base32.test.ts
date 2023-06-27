0b119a2afc3d3e386dc803cce4158a2f4158ea63
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-06-27 18:01:35 +0200

    feat(base32): works for multi words strings

--- app/utils/base32.test.ts
+++ app/utils/base32.test.ts
@@ -1,250 +1,278 @@
-import { describe, expect, test } from "vitest";
+import {
+  asciiToBinary,
+  binaryToDecimal,
+  createBitsGroup,
+  createGroupsOfFive,
+  decimalToSymbol,
+  encodeBase32,
+  replaceEmptyBitsWithZeroes,
+  stringToAscii,
+} from "./_base32";
 
-import { decodeBase32, encodeBase32 } from "~/utils/base32";
-import { encodeUint8 } from "~/utils/uint8";
-
-const encoder = new TextEncoder();
-
-const TEST_CASES: Array<[string, string, string]> = [
-  // RFC 4648 - Test vectors
-  ["RFC4648", "", ""],
-  ["RFC4648", "66", "MY======"],
-  ["RFC4648", "666f", "MZXQ===="],
-  ["RFC4648", "666f6f", "MZXW6==="],
-  ["RFC4648", "666f6f62", "MZXW6YQ="],
-  ["RFC4648", "666f6f6261", "MZXW6YTB"],
-  ["RFC4648", "666f6f626172", "MZXW6YTBOI======"],
-
-  // RFC 4648 - Hex test vectors
-  ["RFC4648-HEX", "", ""],
-  ["RFC4648-HEX", "66", "CO======"],
-  ["RFC4648-HEX", "666f", "CPNG===="],
-  ["RFC4648-HEX", "666f6f", "CPNMU==="],
-  ["RFC4648-HEX", "666f6f62", "CPNMUOG="],
-  ["RFC4648-HEX", "666f6f6261", "CPNMUOJ1"],
-  ["RFC4648-HEX", "666f6f626172", "CPNMUOJ1E8======"],
-
-  // RFC 4648 - Random data
-  ["RFC4648", "73", "OM======"],
-  ["RFC4648", "f80c", "7AGA===="],
-  ["RFC4648", "6450", "MRIA===="],
-  ["RFC4648", "cc91d0", "ZSI5A==="],
-  ["RFC4648", "6c60c0", "NRQMA==="],
-  ["RFC4648", "4f6a23", "J5VCG==="],
-  ["RFC4648", "88b44f18", "RC2E6GA="],
-  ["RFC4648", "90bad04714", "SC5NARYU"],
-  ["RFC4648", "e9ef1def8086", "5HXR334AQY======"],
-  ["RFC4648", "83fe3f9c1e9302", "QP7D7HA6SMBA===="],
-  ["RFC4648", "15aa1f7cafc17cb8", "CWVB67FPYF6LQ==="],
-  ["RFC4648", "da51d4fed48b4c32dc", "3JI5J7WURNGDFXA="],
-  ["RFC4648", "c4be14228512d7299831", "YS7BIIUFCLLSTGBR"],
-  ["RFC4648", "2f273c5b5ef04724fab944", "F4TTYW266BDSJ6VZIQ======"],
-  ["RFC4648", "969da1b80ec2442d2bdd4bdb", "S2O2DOAOYJCC2K65JPNQ===="],
-  ["RFC4648", "31f5adb50792f549d3714f3f99", "GH223NIHSL2UTU3RJ47ZS==="],
-  ["RFC4648", "6a654f7a072c29951930700c0a61", "NJSU66QHFQUZKGJQOAGAUYI="],
-  ["RFC4648", "0fe29d6825ad999e87d9b7cac3589d", "B7RJ22BFVWMZ5B6ZW7FMGWE5"],
-  [
-    "RFC4648",
-    "0f960ab44e165973a5172ccd294b3412",
-    "B6LAVNCOCZMXHJIXFTGSSSZUCI======",
-  ],
-  [
-    "RFC4648",
-    "325b9fd847a41fb0d485c207a1a5b02dcf",
-    "GJNZ7WCHUQP3BVEFYID2DJNQFXHQ====",
-  ],
-  [
-    "RFC4648",
-    "ddf80ebe21bf1b1e12a64c5cc6a74b5d92dd",
-    "3X4A5PRBX4NR4EVGJROMNJ2LLWJN2===",
-  ],
-  [
-    "RFC4648",
-    "c0cae52c6f641ce04a7ee5b9a8fa8ded121bca",
-    "YDFOKLDPMQOOAST64W42R6UN5UJBXSQ=",
-  ],
-  [
-    "RFC4648",
-    "872840a355c8c70586f462c9e669ee760cb3537e",
-    "Q4UEBI2VZDDQLBXUMLE6M2POOYGLGU36",
-  ],
-  [
-    "RFC4648",
-    "5773fe22662818a120c5688824c935fe018208a496",
-    "K5Z74ITGFAMKCIGFNCECJSJV7YAYECFESY======",
-  ],
-  [
-    "RFC4648",
-    "416e23abc524d1b85736e2bea6cfecd5192789034a28",
-    "IFXCHK6FETI3QVZW4K7KNT7M2UMSPCIDJIUA====",
-  ],
-  [
-    "RFC4648",
-    "83d2386ebdd7e8e818ec00e3ccd882aa933b905b7e2e44",
-    "QPJDQ3V527UOQGHMADR4ZWECVKJTXEC3PYXEI===",
-  ],
-  [
-    "RFC4648",
-    "a2fa8b881f3b8024f52745763c4ae08ea12bdf8bef1a72f8",
-    "UL5IXCA7HOACJ5JHIV3DYSXAR2QSXX4L54NHF6A=",
-  ],
-  [
-    "RFC4648",
-    "b074ae8b9efde0f17f37bccadde006d039997b59c8efb05add",
-    "WB2K5C467XQPC7ZXXTFN3YAG2A4ZS62ZZDX3AWW5",
-  ],
-  [
-    "RFC4648",
-    "764fef941aee7e416dc204ae5ab9c5b9ce644567798e6849aea9",
-    "OZH67FA25Z7EC3OCASXFVOOFXHHGIRLHPGHGQSNOVE======",
-  ],
-  [
-    "RFC4648",
-    "4995d9811f37f59797d7c3b9b9e5325aa78277415f70f4accf588c",
-    "JGK5TAI7G72ZPF6XYO43TZJSLKTYE52BL5YPJLGPLCGA====",
-  ],
-  [
-    "RFC4648",
-    "24f0812ca8eed58374c11a7008f0b262698b72fd2792709208eaacb2",
-    "ETYICLFI53KYG5GBDJYAR4FSMJUYW4X5E6JHBEQI5KWLE===",
-  ],
-  [
-    "RFC4648",
-    "d70692543810d4bf50d81cf44a55801a557a388a341367c7ea077ca306",
-    "24DJEVBYCDKL6UGYDT2EUVMADJKXUOEKGQJWPR7KA56KGBQ=",
-  ],
-  [
-    "RFC4648",
-    "6e08a89ca36b677ff8fe99e68a1241c8d8cef2570a5f60b6417d2538b30c",
-    "NYEKRHFDNNTX76H6THTIUESBZDMM54SXBJPWBNSBPUSTRMYM",
-  ],
-  [
-    "RFC4648",
-    "f2fc2319bd29457ccd01e8e194ee9bd7e97298b6610df4ab0f3d5baa0b2d7ccf69829edb74edef",
-    "6L6CGGN5FFCXZTIB5DQZJ3U327UXFGFWMEG7JKYPHVN2UCZNPTHWTAU63N2O33Y=",
-  ],
-
-  // Crockford - Small samples
-  ["Crockford", "", ""],
-  ["Crockford", "61", "C4"],
-  ["Crockford", "74657374", "EHJQ6X0"],
-  ["Crockford", "6c696e7573", "DHMPWXBK"],
-  ["Crockford", "666f6f626172", "CSQPYRK1E8"],
-];
-
-const CROCKFORD_EXTRAS: Array<[string, string, string]> = [
-  ["Crockford", "", ""],
-  ["Crockford", "61", "C4"],
-  ["Crockford", "61", "c4"],
-  ["Crockford", "74657374", "EHJQ6X0"],
-  ["Crockford", "74657374", "EHJQ6XO"],
-  ["Crockford", "6c696e7573", "DHMPWXBK"],
-  ["Crockford", "6c696e7573", "DhmPWXbK"],
-  ["Crockford", "666f6f626172", "CSQPYRK1E8"],
-  ["Crockford", "666f6f626172", "CSQPYRKLE8"],
-  ["Crockford", "666f6f626172", "CSQPYRKIE8"],
-];
-
-describe("encode", () => {
-  TEST_CASES.forEach(([variant, input, expected]) => {
-    test(`should encode ${variant} ${input}`, () => {
-      expect(encodeBase32(encodeUint8(input), variant as any)).toEqual(
-        expected
-      );
-    });
+describe("encodeBase32", () => {
+  test('encodes "Cat" correctly', () => {
+    expect(encodeBase32("Cat")).toBe("INQXI===");
   });
 
-  TEST_CASES.forEach(([variant, input, expected]) => {
-    test(`should encode w/ padding disabled ${variant} ${input}`, () => {
-      const options = { padding: false };
-      expect(encodeBase32(encodeUint8(input), variant as any, options)).toEqual(
-        // eslint-disable-next-line no-useless-escape
-        expected.replace(/\=/g, "")
-      );
-    });
+  test('encodes "Hello World" correctly', () => {
+    expect(encodeBase32("Hello World")).toBe("JBSWY3DPEBLW64TMMQ======");
   });
 
-  test("should encode simple examples", () => {
-    expect(encodeBase32(encoder.encode("a"))).toBe("ME======");
-    expect(encodeBase32(encoder.encode("be"))).toBe("MJSQ====");
-    expect(encodeBase32(encoder.encode("bee"))).toBe("MJSWK===");
-    expect(encodeBase32(encoder.encode("beer"))).toBe("MJSWK4Q=");
-    expect(encodeBase32(encoder.encode("beers"))).toBe("MJSWK4TT");
-    expect(encodeBase32(encoder.encode("beers 1"))).toBe("MJSWK4TTEAYQ====");
-    expect(encodeBase32(encoder.encode("shockingly dismissed"))).toBe(
-      "ONUG6Y3LNFXGO3DZEBSGS43NNFZXGZLE"
+  test('encodes "Qwik is amazing!" correctly', () => {
+    expect(encodeBase32("Qwik is amazing!")).toBe(
+      "KF3WS2ZANFZSAYLNMF5GS3THEE======"
     );
   });
-  test("should error on unsupported variant", () => {
-    expect(() => encodeBase32(encoder.encode("a"), "fail" as any)).toThrow(
-      "Unknown base32 variant: fail"
+
+  test('encodes "A new web application for torrents is coming, just wait for it" correctly', () => {
+    expect(
+      encodeBase32(
+        "A new web application for torrents is coming, just wait for it"
+      )
+    ).toBe(
+      "IEQG4ZLXEB3WKYRAMFYHA3DJMNQXI2LPNYQGM33SEB2G64TSMVXHI4ZANFZSAY3PNVUW4ZZMEBVHK43UEB3WC2LUEBTG64RANF2A===="
     );
   });
 });
 
-describe("decode", () => {
-  TEST_CASES.forEach(([variant, input, expected]) => {
-    test(`should decode ${variant} ${input}`, () => {
-      expect(decodeBase32(expected, variant as any)).toEqual(
-        encodeUint8(input)
-      );
-    });
-  });
-  CROCKFORD_EXTRAS.forEach(([variant, input, expected]) => {
-    test(`should decode crockford extra ${variant} ${input}`, () => {
-      expect(decodeBase32(expected, variant as any)).toEqual(
-        encodeUint8(input)
-      );
-    });
+describe("stringToAscii", () => {
+  test('encodes "Cat" correctly', () => {
+    expect(stringToAscii("Cat")).toStrictEqual([67, 97, 116]);
   });
 
-  test.only("should decode simple examples", () => {
-    console.log(
-      'test ~ decodeBase32("ME======"):',
-      decodeBase32("ME======").toString()
-    );
+  test('encodes "Hello World" correctly', () => {
+    expect(stringToAscii("Hello World")).toStrictEqual([
+      72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100,
+    ]);
+  });
+});
 
-    console.log(
-      'test ~ decodeBase32("ME======"):',
-      encoder.encode(decodeBase32("ME======").toString())
-    );
-    expect(decodeBase32("ME======").toString()).toBe("a");
-    // expect(encoder.encode(decodeBase32("MJSQ====")).toString()).toBe("be");
-    // expect(encoder.encode(decodeBase32("ONXW4===")).toString()).toBe("son");
-    // expect(encoder.encode(decodeBase32("MJSWK===")).toString()).toBe("bee");
-    // expect(encoder.encode(decodeBase32("MJSWK4Q=")).toString()).toBe("beer");
-    // expect(encoder.encode(decodeBase32("MJSWK4TT")).toString()).toBe("beers");
-    // expect(encoder.encode(decodeBase32("mjswK4TT")).toString()).toBe("beers");
-    // expect(encoder.encode(decodeBase32("MJSWK4TTN5XA====")).toString()).toBe(
-    //   "beerson"
-    // );
-    // expect(encoder.encode(decodeBase32("MJSWK4TTEAYQ====")).toString()).toBe(
-    //   "beers 1"
-    // );
-    // expect(
-    //   encoder
-    //     .encode(decodeBase32("ONUG6Y3LNFXGO3DZEBSGS43NNFZXGZLE"))
-    //     .toString()
-    // ).toBe("shockingly dismissed");
+describe("asciiToBinary", () => {
+  test("encodes [67, 97, 116] correctly", () => {
+    expect(asciiToBinary([67, 97, 116])).toStrictEqual([
+      "01000011",
+      "01100001",
+      "01110100",
+    ]);
   });
 
-  test("should be binary safe", () => {
-    expect(encodeBase32(new Uint8Array([0x00, 0xff, 0x88])).toString()).toBe(
-      "00ff88"
-    );
-    const code = "f61e1f998d69151de8334dbe753ab17ae831c13849a6aecd95d0a4e5dc25";
-    const encoded = "6YPB7GMNNEKR32BTJW7HKOVRPLUDDQJYJGTK5TMV2CSOLXBF";
-    expect(encodeBase32(encoder.encode(code)).toString()).toBe(encoded);
-    expect(decodeBase32(encoded).toString()).toBe(code);
+  test("encodes [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100] correctly", () => {
+    expect(
+      asciiToBinary([72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100])
+    ).toStrictEqual([
+      "01001000",
+      "01100101",
+      "01101100",
+      "01101100",
+      "01101111",
+      "00100000",
+      "01010111",
+      "01101111",
+      "01110010",
+      "01101100",
+      "01100100",
+    ]);
+  });
+});
+
+describe("createGroupsOfFive", () => {
+  test(`creates the group for ["01000011", "01100001", "01110100"] correctly`, () => {
+    expect(
+      createGroupsOfFive(["01000011", "01100001", "01110100"])
+    ).toStrictEqual([
+      ["01000011", "01100001", "01110100", "xxxxxxxx", "xxxxxxxx"],
+    ]);
   });
 
-  test("should error on unsupported variant", () => {
-    expect(() => decodeBase32("ME======", "fail" as any)).toThrow(
-      "Unknown base32 variant: fail"
-    );
+  test(`creates the group for ["01001000", "01100101", "01101100", "01101100", "01101111", "00100000", "01010111", "01101111", "01110010", "01101100", "01100100"] correctly`, () => {
+    expect(
+      createGroupsOfFive([
+        "01001000",
+        "01100101",
+        "01101100",
+        "01101100",
+        "01101111",
+        "00100000",
+        "01010111",
+        "01101111",
+        "01110010",
+        "01101100",
+        "01100100",
+      ])
+    ).toStrictEqual([
+      ["01001000", "01100101", "01101100", "01101100", "01101111"],
+      ["00100000", "01010111", "01101111", "01110010", "01101100"],
+      ["01100100", "xxxxxxxx", "xxxxxxxx", "xxxxxxxx", "xxxxxxxx"],
+    ]);
+  });
+});
+
+describe("createBitsGroup", () => {
+  test(`creates group ["01000011", "01100001", "01110100", "xxxxxxxx", "xxxxxxxx"] correctly`, () => {
+    expect(
+      createBitsGroup([
+        ["01000011", "01100001", "01110100", "xxxxxxxx", "xxxxxxxx"],
+      ])
+    ).toStrictEqual([
+      ["01000", "01101", "10000", "10111", "0100x", "xxxxx", "xxxxx", "xxxxx"],
+    ]);
   });
 
-  test("should error from invalid encoding characters", () => {
-    expect(() => decodeBase32("M😴")).toThrow("Invalid character found: ");
+  test(`creates group ["01000011", "01100001", "01110100", "xxxxxxxx", "xxxxxxxx"] correctly`, () => {
+    expect(
+      createBitsGroup([
+        ["01001000", "01100101", "01101100", "01101100", "01101111"],
+        ["00100000", "01010111", "01101111", "01110010", "01101100"],
+        ["01100100", "xxxxxxxx", "xxxxxxxx", "xxxxxxxx", "xxxxxxxx"],
+      ])
+    ).toStrictEqual([
+      ["01001", "00001", "10010", "10110", "11000", "11011", "00011", "01111"],
+      ["00100", "00001", "01011", "10110", "11110", "11100", "10011", "01100"],
+      ["01100", "100xx", "xxxxx", "xxxxx", "xxxxx", "xxxxx", "xxxxx", "xxxxx"],
+    ]);
+  });
+});
+
+describe("replaceEmptyBitsWithZeroes", () => {
+  test(`replaces zeroes for ["01000", "01101", "10000", "10111", "0100x", "xxxxx", "xxxxx", "xxxxx"] correctly`, () => {
+    expect(
+      replaceEmptyBitsWithZeroes([
+        [
+          "01000",
+          "01101",
+          "10000",
+          "10111",
+          "0100x",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+        ],
+      ])
+    ).toStrictEqual([
+      ["01000", "01101", "10000", "10111", "01000", "xxxxx", "xxxxx", "xxxxx"],
+    ]);
+  });
+
+  test(`replaces zeroes for ["01000", "01101", "10000", "10111", "0100x", "xxxxx", "xxxxx", "xxxxx"] correctly`, () => {
+    expect(
+      replaceEmptyBitsWithZeroes([
+        [
+          "01001",
+          "00001",
+          "10010",
+          "10110",
+          "11000",
+          "11011",
+          "00011",
+          "01111",
+        ],
+        [
+          "00100",
+          "00001",
+          "01011",
+          "10110",
+          "11110",
+          "11100",
+          "10011",
+          "01100",
+        ],
+        [
+          "01100",
+          "100xx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+        ],
+      ])
+    ).toStrictEqual([
+      ["01001", "00001", "10010", "10110", "11000", "11011", "00011", "01111"],
+      ["00100", "00001", "01011", "10110", "11110", "11100", "10011", "01100"],
+      ["01100", "10000", "xxxxx", "xxxxx", "xxxxx", "xxxxx", "xxxxx", "xxxxx"],
+    ]);
+  });
+});
+
+describe("binaryToDecimal", () => {
+  test(`replaces zeroes for ["01000", "01101", "10000", "10111", "01000", "xxxxx", "xxxxx", "xxxxx"] correctly`, () => {
+    expect(
+      binaryToDecimal([
+        [
+          "01000",
+          "01101",
+          "10000",
+          "10111",
+          "01000",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+        ],
+      ])
+    ).toStrictEqual([[8, 13, 16, 23, 8, "=", "=", "="]]);
+  });
+
+  test(`replaces zeroes for ["01000", "01101", "10000", "10111", "01000", "xxxxx", "xxxxx", "xxxxx"] correctly`, () => {
+    expect(
+      binaryToDecimal([
+        [
+          "01001",
+          "00001",
+          "10010",
+          "10110",
+          "11000",
+          "11011",
+          "00011",
+          "01111",
+        ],
+        [
+          "00100",
+          "00001",
+          "01011",
+          "10110",
+          "11110",
+          "11100",
+          "10011",
+          "01100",
+        ],
+        [
+          "01100",
+          "10000",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+          "xxxxx",
+        ],
+      ])
+    ).toStrictEqual([
+      [9, 1, 18, 22, 24, 27, 3, 15],
+      [4, 1, 11, 22, 30, 28, 19, 12],
+      [12, 16, "=", "=", "=", "=", "=", "="],
+    ]);
+  });
+});
+
+describe("decimalToSymbol", () => {
+  test(`replaces zeroes for [8, 13, 16, 23, 8, "=", "=", "="] correctly`, () => {
+    expect(decimalToSymbol([[8, 13, 16, 23, 8, "=", "=", "="]])).toStrictEqual([
+      ["I", "N", "Q", "X", "I", "=", "=", "="],
+    ]);
+  });
+
+  test(`replaces zeroes for [8, 13, 16, 23, 8, "=", "=", "="] correctly`, () => {
+    expect(
+      decimalToSymbol([
+        [9, 1, 18, 22, 24, 27, 3, 15],
+        [4, 1, 11, 22, 30, 28, 19, 12],
+        [12, 16, "=", "=", "=", "=", "=", "="],
+      ])
+    ).toStrictEqual([
+      ["J", "B", "S", "W", "Y", "3", "D", "P"],
+      ["E", "B", "L", "W", "6", "4", "T", "M"],
+      ["M", "Q", "=", "=", "=", "=", "=", "="],
+    ]);
   });
 });
--- app/utils/base32.ts
+++ app/utils/base32.ts
@@ -1,123 +1,167 @@
-const RFC4648 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-const RFC4648_HEX = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
-const CROCKFORD = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+export function encodeBase32(string: string) {
+  // word is "Cat"
+  //
+  // 1. convert each character into ASCII decimal values
+  // [67, 97, 116]
+  const ascii = stringToAscii(string);
+  //
+  // 2.	convert each ASCII decimal value into bytes
+  // [01000011, 01100001, 01110100]
+  const bytes = asciiToBinary(ascii);
+  //
+  // 3. convert the bytes stream into groups of 5
+  // const group = [01000011, 01100001, 01110100, xxxxxxxx, xxxxxxxx]
+  const chunks = createGroupsOfFive(bytes);
+  //
+  // 4. divide group into 8 chunks of 5 bits
+  // const chunks = [01000, 01101, 10000, 10111, 0100x, xxxxx, xxxxx, xxxxx]
+  const nextChunks = createBitsGroup(chunks);
+  //
+  // 5. if a chunk has both bits and empty bits, replace the empty bits with 0's (zeros)
+  // const chunks = [01000, 01101, 10000, 10111, 01000, xxxxx, xxxxx, xxxxx]
+  const filledChunks = replaceEmptyBitsWithZeroes(nextChunks);
+  //
+  // 6. convert each 5 bits chunk to its decimal value (0-31),
+  //    if a chunk contains empty bits, replace with character "="
+  // const chunks = [8, 13, 16, 23, 8, =, =, =]
+  const decimals = binaryToDecimal(filledChunks);
+  //
+  // 7. in the base-32 symbol chart, map each decimal value to its corresponding character
+  // const chunks = [I, N, Q, X, I, =, =, =]
+  const symbols = decimalToSymbol(decimals);
+  // excellent explanatory video
+  // https://www.youtube.com/watch?v=Va8FLD-iuTg
 
-type Variant = "RFC3548" | "RFC4648" | "RFC4648-HEX" | "Crockford";
-
-export function encodeBase32(
-  buffer: Uint8Array,
-  variant: Variant = "RFC4648",
-  options: Partial<{ padding: boolean }> = {}
-): string {
-  let alphabet: string;
-  let defaultPadding: boolean;
-
-  switch (variant) {
-    case "RFC3548":
-    case "RFC4648":
-      alphabet = RFC4648;
-      defaultPadding = true;
-      break;
-    case "RFC4648-HEX":
-      alphabet = RFC4648_HEX;
-      defaultPadding = true;
-      break;
-    case "Crockford":
-      alphabet = CROCKFORD;
-      defaultPadding = false;
-      break;
-    default:
-      throw new Error(`Unknown base32 variant: ${variant as string}`);
-  }
-
-  const padding = options.padding ?? defaultPadding;
-  const length = buffer.byteLength;
-  const view = new Uint8Array(buffer);
-
-  let bits = 0;
-  let value = 0;
-  let output = "";
-
-  for (let i = 0; i < length; i++) {
-    value = (value << 8) | view[i]!;
-    bits += 8;
-
-    while (bits >= 5) {
-      output += alphabet[(value >>> (bits - 5)) & 31];
-      bits -= 5;
-    }
-  }
-
-  if (bits > 0) {
-    output += alphabet[(value << (5 - bits)) & 31];
-  }
-
-  if (padding) {
-    while (output.length % 8 !== 0) {
-      output += "=";
-    }
-  }
-
-  return output;
+  return symbols.flatMap((symbol) => symbol.join("")).join("");
 }
 
-function readChar(alphabet: string, char: string): number {
-  const idx = alphabet.indexOf(char);
-
-  if (idx === -1) {
-    throw new Error("Invalid character found: " + char);
-  }
-
-  return idx;
+// 1
+export function stringToAscii(string: string): number[] {
+  return [...string].map((c) => c.charCodeAt(0));
 }
 
-export function decodeBase32(
-  input: string,
-  variant: Variant = "RFC4648"
-): Uint8Array {
-  let alphabet: string;
-  let cleanedInput: string;
-
-  switch (variant) {
-    case "RFC3548":
-    case "RFC4648":
-      alphabet = RFC4648;
-      cleanedInput = input.toUpperCase().replace(/=+$/, "");
-      break;
-    case "RFC4648-HEX":
-      alphabet = RFC4648_HEX;
-      cleanedInput = input.toUpperCase().replace(/=+$/, "");
-      break;
-    case "Crockford":
-      alphabet = CROCKFORD;
-      cleanedInput = input
-        .toUpperCase()
-        .replace(/O/g, "0")
-        .replace(/[IL]/g, "1");
-      break;
-    default:
-      throw new Error(`Unknown base32 variant: ${variant as string}`);
-  }
-
-  const { length } = cleanedInput;
-  console.log("cleanedInput:", cleanedInput);
-
-  let bits = 0;
-  let value = 0;
-
-  let index = 0;
-  const output = new Uint8Array(((length * 5) / 8) | 0);
-
-  for (let i = 0; i < length; i++) {
-    console.log("cleanedInput[i]:", cleanedInput[i]);
-    value = (value << 5) | readChar(alphabet, cleanedInput[i]!);
-    bits += 5;
-
-    if (bits >= 8) {
-      output[index++] = (value >>> (bits - 8)) & 255;
-      bits -= 8;
-    }
-  }
-
-  return output;
+// 2
+export function asciiToBinary(asciis: number[]): string[] {
+  return asciis.map((num) => num.toString(2).padStart(8, "0"));
 }
+
+// 3
+export function createGroupsOfFive(binaries: string[]): string[][] {
+  const groups = binaries
+    .reduce<string[][]>((prevGroups, c) => {
+      const lastChunk = prevGroups[prevGroups.length - 1] ?? [];
+
+      if (lastChunk.length === 5) {
+        return [...prevGroups, [c]];
+      } else {
+        return [...prevGroups.slice(0, -1), [...lastChunk, c]];
+      }
+    }, [])
+    .map((group) => {
+      return Array.from({ length: 5 }).map((_, i) => group[i] ?? "xxxxxxxx");
+    });
+
+  return groups;
+}
+
+// 4
+export function createBitsGroup(groups: string[][]): string[][] {
+  const chunks = groups.map((group) => {
+    return [...group.join("")]
+      .reduce<string[][]>((prevChunks, c) => {
+        const lastChunk = prevChunks[prevChunks.length - 1] ?? [];
+
+        if (lastChunk.length === 5) {
+          return [...prevChunks, [c]];
+        } else {
+          return [...prevChunks.slice(0, -1), [...lastChunk, c]];
+        }
+      }, [])
+      .map((chunk) => {
+        if (chunk.length === 5) {
+          return chunk;
+        } else {
+          return Array.from({ length: 5 }).map((_, i) => chunk[i] ?? "x");
+        }
+      })
+      .map((chunk) => chunk.join(""));
+  });
+
+  return chunks;
+}
+
+// 5
+export function replaceEmptyBitsWithZeroes(chunks: string[][]): string[][] {
+  return chunks.map((_chunk) => {
+    return _chunk.map((chunk) => {
+      if (chunk[0] === "x") {
+        return chunk;
+      } else {
+        return Array.from({ length: 5 })
+          .map((_, i) => (chunk[i] !== "x" ? chunk[i] : "0"))
+          .join("");
+      }
+    });
+  });
+}
+
+// 6
+export function binaryToDecimal(chunks: string[][]): (number | string)[][] {
+  return chunks.map((_chunk) => {
+    return _chunk.map((chunk) => {
+      if (chunk[0] === "x") {
+        return "=";
+      } else {
+        return parseInt(chunk, 2);
+      }
+    });
+  });
+}
+
+// 7
+export function decimalToSymbol(decimals: (number | string)[][]): string[][] {
+  return decimals.map((_decimal) => {
+    return _decimal.map((decimal) => {
+      if (decimal === "=") {
+        return decimal;
+      } else {
+        return BASE_32_SYMBOLS[decimal as number];
+      }
+    });
+  });
+}
+
+const BASE_32_SYMBOLS: { [key: number]: string } = {
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+  7: "H",
+  8: "I",
+  9: "J",
+  10: "K",
+  11: "L",
+  12: "M",
+  13: "N",
+  14: "O",
+  15: "P",
+  16: "Q",
+  17: "R",
+  18: "S",
+  19: "T",
+  20: "U",
+  21: "V",
+  22: "W",
+  23: "X",
+  24: "Y",
+  25: "Z",
+  26: "2",
+  27: "3",
+  28: "4",
+  29: "5",
+  30: "6",
+  31: "7",
+};
